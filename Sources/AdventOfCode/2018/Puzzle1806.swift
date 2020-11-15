0f81e0259423a707ec0b56e257f6b415bb1a8899
Avery Pierce <avery.pierce@pm.me>
2020-11-15 12:33:07 -0600

    Add test case

--- Sources/AdventOfCode/2018/Puzzle1806.swift
+++ Sources/AdventOfCode/2018/Puzzle1806.swift
@@ -9,7 +9,14 @@ class Puzzle1806: Puzzle {
     }
     
     var testCasesA: [TestCase] = [
-        // Input test cases here
+        TestCase(TextInput("""
+                    1, 1
+                    1, 6
+                    8, 3
+                    3, 4
+                    5, 5
+                    8, 9
+                    """), expectedOutput: "17")
     ]
     
     func solveB(_ input: Input) -> String {
