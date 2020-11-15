ebc307c27ce2d6407cd4c5072502070b7f7964a7
Avery Pierce <avery.pierce@pm.me>
2020-11-15 14:37:54 -0600

    Solve 2018_07 part 1

--- Sources/AdventOfCode/2018/Puzzle1807.swift
+++ Sources/AdventOfCode/2018/Puzzle1807.swift
@@ -5,7 +5,17 @@ class Puzzle1807: Puzzle {
     let name: String = "2018_07"
     
     func solveA(_ input: Input) -> String {
-        return "unsolved"
+        let rules = input.lines.map(Rule.init(descriptor:))
+        let graph = Graph(rules)
+        
+        var output = ""
+        repeat {
+            let nextStep = graph.nextStep
+            output += nextStep
+            graph.completeStep(nextStep)
+        } while !graph.incompleteSteps.isEmpty
+        
+        return output
     }
     
     var testCasesA: [TestCase] = [
@@ -27,4 +37,64 @@ class Puzzle1807: Puzzle {
     var testCasesB: [TestCase] = [
         // Input test cases here
     ]
+    
+    class Graph {
+        var rules: [Rule]
+        var completedSteps: Set<String> = Set()
+        
+        lazy var allSteps: Set<String> = {
+            var allSteps = Set<String>()
+            for rule in rules {
+                allSteps.insert(rule.before)
+                allSteps.insert(rule.after)
+            }
+            return allSteps
+        }()
+        
+        var incompleteSteps: Set<String> {
+            return allSteps.subtracting(completedSteps)
+        }
+        
+        init(_ rules: [Rule]) {
+            self.rules = rules
+        }
+        
+        var nextStep: String {
+            return unblockedSteps.sorted()[0]
+        }
+        
+        var unblockedSteps: [String] {
+            return allSteps
+                .filter({ isStepUnblocked($0) })
+                .filter({ !completedSteps.contains($0) })
+        }
+        
+        func isStepUnblocked(_ step: String) -> Bool {
+            let rulesBlockingStep = rules
+                .filter({ $0.after == step })
+                .filter({ !completedSteps.contains($0.before) })
+            
+            return rulesBlockingStep.count == 0
+        }
+        
+        func completeStep(_ step: String) {
+            completedSteps.insert(step)
+        }
+    }
+    
+    struct Rule {
+        var before: String
+        var after: String
+        
+        init(before: String, after: String) {
+            self.before = before
+            self.after = after
+        }
+        
+        init(descriptor: String) {
+            let words = descriptor.split(separator: " ")
+            self.before = String(words[1])
+            self.after = String(words[7])
+        }
+    }
 }
