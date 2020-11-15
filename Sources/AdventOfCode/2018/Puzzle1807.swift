7c29bdf9d7d0f056da534878537819b61ba718a5
Avery Pierce <avery.pierce@pm.me>
2020-11-15 14:07:26 -0600

    Add test case for 2018_07

--- Sources/AdventOfCode/2018/Puzzle1807.swift
+++ Sources/AdventOfCode/2018/Puzzle1807.swift
@@ -9,7 +9,15 @@ class Puzzle1807: Puzzle {
     }
     
     var testCasesA: [TestCase] = [
-        // Input test cases here
+        TestCase(TextInput("""
+                    Step C must be finished before step A can begin.
+                    Step C must be finished before step F can begin.
+                    Step A must be finished before step B can begin.
+                    Step A must be finished before step D can begin.
+                    Step B must be finished before step E can begin.
+                    Step D must be finished before step E can begin.
+                    Step F must be finished before step E can begin.
+                    """), expectedOutput: "CABDFE")
     ]
     
     func solveB(_ input: Input) -> String {
