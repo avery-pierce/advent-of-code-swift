93ae976002d6606aa5276b86df14103214edbf90
Avery Pierce <avery.pierce@pm.me>
2020-11-15 14:40:49 -0600

    Add test cases for part 2

--- Sources/AdventOfCode/2018/Puzzle1807.swift
+++ Sources/AdventOfCode/2018/Puzzle1807.swift
@@ -31,11 +31,23 @@ class Puzzle1807: Puzzle {
     ]
     
     func solveB(_ input: Input) -> String {
+        let isTesting = input.lines.count == 7
+        let numWorkers = isTesting ? 2 : 5
+        let baseWorkTimePerStep = isTesting ? 0 : 60
+        
         return "unsolved"
     }
     
     var testCasesB: [TestCase] = [
-        // Input test cases here
+        TestCase(TextInput("""
+                    Step C must be finished before step A can begin.
+                    Step C must be finished before step F can begin.
+                    Step A must be finished before step B can begin.
+                    Step A must be finished before step D can begin.
+                    Step B must be finished before step E can begin.
+                    Step D must be finished before step E can begin.
+                    Step F must be finished before step E can begin.
+                    """), expectedOutput: "15")
     ]
     
     class Graph {
