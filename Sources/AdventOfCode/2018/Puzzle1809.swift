966954a759b4068a41b1c2b186abe1f07e62786f
Avery Pierce <avery.pierce@pm.me>
2020-11-16 09:15:30 -0600

    Add test cases for 2018_09

--- Sources/AdventOfCode/2018/Puzzle1809.swift
+++ Sources/AdventOfCode/2018/Puzzle1809.swift
@@ -9,7 +9,11 @@ class Puzzle1809: Puzzle {
     }
     
     var testCasesA: [TestCase] = [
-        // Input test cases here
+        TestCase(TextInput("10 players; last marble is worth 1618 points"), expectedOutput: "8317"),
+        TestCase(TextInput("13 players; last marble is worth 7999 points"), expectedOutput: "146373"),
+        TestCase(TextInput("17 players; last marble is worth 1104 points"), expectedOutput: "2764"),
+        TestCase(TextInput("21 players; last marble is worth 6111 points"), expectedOutput: "54718"),
+        TestCase(TextInput("30 players; last marble is worth 5807 points"), expectedOutput: "37305"),
     ]
     
     func solveB(_ input: Input) -> String {
