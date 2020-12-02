7c1eea4cc6fd08a23ed3df4c65c03470414171cd
Avery Pierce <avery.pierce@pm.me>
2020-12-02 08:09:46 -0600

    complete 2020 day 2

--- Sources/AdventOfCode/2020/Puzzle2001.swift
+++ Sources/AdventOfCode/2020/Puzzle2001.swift
@@ -5,18 +5,52 @@ class Puzzle2001: Puzzle {
     let name: String = "2020_01"
     
     func solveA(_ input: Input) -> String {
-        return "unsolved"
+        let ints = input.lines.compactMap(Int.init)
+        for a in ints {
+            for b in ints {
+                if a != b && a + b == 2020 {
+                    return "\(a*b)"
+                }
+            }
+        }
+        return "??"
     }
     
     var testCasesA: [TestCase] = [
-        // Input test cases here
+        TestCase(TextInput("""
+                    1721
+                    979
+                    366
+                    299
+                    675
+                    1456
+                    """), expectedOutput: "514579")
     ]
     
     func solveB(_ input: Input) -> String {
-        return "unsolved"
+        let ints = input.lines.compactMap(Int.init)
+        for a in ints {
+            for b in ints {
+                if a != b {
+                    for c in ints {
+                        if a != c && b != c && a + b + c == 2020 {
+                            return "\(a * b * c)"
+                        }
+                    }
+                }
+            }
+        }
+        return ""
     }
     
     var testCasesB: [TestCase] = [
-        // Input test cases here
+        TestCase(TextInput("""
+                    1721
+                    979
+                    366
+                    299
+                    675
+                    1456
+                    """), expectedOutput: "241861950")
     ]
 }
