81a05c09d584fa55a24db93475a182c397dce55a
Avery Pierce <avery.pierce@pm.me>
2020-12-04 09:08:47 -0600

    2018 day 20 first attempts

--- Sources/AdventOfCode/2018/Puzzle1820.swift
+++ Sources/AdventOfCode/2018/Puzzle1820.swift
@@ -1,15 +1,22 @@
 
 import Foundation
 
+let regex = try! NSRegularExpression(pattern: "\\(([NSEW]*)\\|([NSEW]*)(?:\\|([NSEW]*))?\\)", options: [])
+
 class Puzzle1820: Puzzle {
     let name: String = "2018_20"
     
     func solveA(_ input: Input) -> String {
-        return "unsolved"
+        let result = findLongestPath(input.text)
+        print(result)
+        return "\(result.count - 2)"
     }
     
     var testCasesA: [TestCase] = [
-        // Input test cases here
+        TestCase(TextInput("^WNE$"), expectedOutput: "3"),
+        TestCase(TextInput("^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$"), expectedOutput: "23"),
+        TestCase(TextInput("^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$"), expectedOutput: "31"),
+        TestCase(TextInput("^WNE(|NN|EEE)$"), expectedOutput: "6"),
     ]
     
     func solveB(_ input: Input) -> String {
@@ -19,4 +26,59 @@ class Puzzle1820: Puzzle {
     var testCasesB: [TestCase] = [
         // Input test cases here
     ]
+    
+    func uniquePaths(_ directions: String) -> Set<String> {
+        if (!directions.contains("(")) {
+            return Set([directions])
+        }
+        
+        let fullStringRange = NSRange(location: 0, length: directions.count)
+        guard let firstMatch = regex.firstMatch(in: directions, options: [], range: fullStringRange) else {
+            return Set()
+        }
+        
+        let ranges = (0..<firstMatch.numberOfRanges).map { (index) in
+            return firstMatch.range(at: index)
+        }
+        
+        let nsString = NSString(string: directions)
+       
+        let placeholderRange = ranges[0]
+        let optA = nsString.substring(with: ranges[1])
+        let optB = nsString.substring(with: ranges[2])
+        
+        let resultA = nsString.replacingCharacters(in: placeholderRange, with: optA)
+        let resultB = nsString.replacingCharacters(in: placeholderRange, with: optB)
+        
+        let set = uniquePaths(resultA)
+        return set.union(uniquePaths(resultB))
+    }
+    
+    func findLongestPath(_ directions: String) -> String {
+        if (!directions.contains("(")) {
+            return directions
+        }
+        
+        let fullStringRange = NSRange(location: 0, length: directions.count)
+        guard let firstMatch = regex.firstMatch(in: directions, options: [], range: fullStringRange) else {
+            return directions
+        }
+        
+        let ranges = (0..<firstMatch.numberOfRanges).map { (index) in
+            return firstMatch.range(at: index)
+        }
+        print(ranges)
+        let nsString = NSString(string: directions)
+        
+        let placeholderRange = ranges[0]
+        var longerRange: NSRange = (ranges[1].length > ranges[2].length) ? ranges[1] : ranges[2]
+        if (ranges.count > 3 && ranges[3].length > longerRange.length) {
+            longerRange = ranges[3]
+        }
+        
+        let longerString = nsString.substring(with: longerRange)
+        
+        let result = nsString.replacingCharacters(in: placeholderRange, with: longerString)
+        return findLongestPath(result)
+    }
 }
