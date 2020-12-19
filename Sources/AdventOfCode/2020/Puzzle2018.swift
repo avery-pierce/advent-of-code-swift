b7f4195d9d7cb20e6067d2cd0e1839343e4f54ee
Avery Pierce <avery.pierce@pm.me>
2020-12-19 10:15:23 -0600

    Solve day 18

--- Sources/AdventOfCode/2020/Puzzle2018.swift
+++ Sources/AdventOfCode/2020/Puzzle2018.swift
@@ -20,11 +20,15 @@ class Puzzle2018: Puzzle {
     ]
     
     func solveB(_ input: Input) -> String {
-        return "unsolved"
+        let sum = input.lines.map(solve2(expression:)).reduce(0, +)
+        return "\(sum)"
     }
     
     var testCasesB: [TestCase] = [
-        // Input test cases here
+        TestCase(TextInput("2 * 3 + (4 * 5)"), expectedOutput: "46"),
+        TestCase(TextInput("5 + (8 * 3 + 9 + 3 * 4 * 3)"), expectedOutput: "1445"),
+        TestCase(TextInput("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), expectedOutput: "669060"),
+        TestCase(TextInput("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"), expectedOutput: "23340"),
     ]
     
     func solve(expression: String) -> Int {
@@ -45,6 +49,42 @@ class Puzzle2018: Puzzle {
             let nsString = NSString(string: expression)
             let replaced = nsString.replacingCharacters(in: wrapRange, with: "\(result)")
             return solve(expression: replaced)
+        }
+        
+        guard let output = Int(expression) else {
+            print(expression)
+            return 0
+        }
+        return output
+    }
+    
+    func solve2(expression: String) -> Int {
+        if let group = regexParseRange("(\\(([^\\(\\)]+)\\))")(expression), !group.isEmpty {
+            let (_, wrapRange) = group[0]
+            let (subExpression, _) = group[1]
+            
+            let result = solve2(expression: subExpression)
+            let nsString = NSString(string: expression)
+            let replaced = nsString.replacingCharacters(in: wrapRange, with: "\(result)")
+            return solve2(expression: replaced)
+        }
+        
+        if let firstAddition = regexParseRange("((\\d+)\\s(\\+)\\s(\\d+))")(expression), !firstAddition.isEmpty {
+            let (_, wrapRange) = firstAddition[0]
+            let result = compute(firstAddition[1...].map(\.0))
+            
+            let nsString = NSString(string: expression)
+            let replaced = nsString.replacingCharacters(in: wrapRange, with: "\(result)")
+            return solve2(expression: replaced)
+        }
+        
+        if let firstOperator = regexParseRange("^((\\d+)\\s(\\*)\\s(\\d+))")(expression), !firstOperator.isEmpty {
+            let (_, wrapRange) = firstOperator[0]
+            let result = compute(firstOperator[1...].map(\.0))
+            
+            let nsString = NSString(string: expression)
+            let replaced = nsString.replacingCharacters(in: wrapRange, with: "\(result)")
+            return solve2(expression: replaced)
         }
         
         guard let output = Int(expression) else {
