d2f7db2f0691ffc87c60a7d4cc515b3305f7819e
Avery Pierce <avery.pierce@pm.me>
2020-12-23 10:23:30 -0600

    WIP

--- Sources/AdventOfCode/2020/Puzzle2023.swift
+++ Sources/AdventOfCode/2020/Puzzle2023.swift
@@ -5,9 +5,12 @@ class Puzzle2023: Puzzle {
     let name: String = "2020_23"
     
     func solveA(_ input: Input) -> String {
-        let sim = CupSimulation(input.text)
-        sim.do100Moves()
-        return sim.result
+        let cups = input.text.map(String.init).compactMap(Int.init)
+        let sim = CupSimulation2(cups)
+        for _ in (0..<100) {
+            sim.doMove()
+        }
+        return sim.solve1()
     }
     
     var testCasesA: [TestCase] = [
@@ -17,11 +20,13 @@ class Puzzle2023: Puzzle {
     func solveB(_ input: Input) -> String {
         var cups = input.text.map(String.init).compactMap(Int.init)
         cups.append(contentsOf: ((cups.max()! + 1)..<1_000_000))
-        let sim = CupSimulation(cups)
+        print("Number of cups", cups.count)
+        
+        let sim = CupSimulation2(cups)
         
         for i in 0..<10_000_000 {
             sim.doMove()
-            if i % 10_000 == 0 {
+            if i % 100 == 0 {
                 print("Move \(i)")
             }
         }
@@ -33,94 +38,89 @@ class Puzzle2023: Puzzle {
         TestCase(TextInput("389125467"), expectedOutput: "149245887792")
     ]
     
-    class CupSimulation {
-        var cups: [Int]
+    class CupSimulation2 {
         var currentCup: Int
-        
-        convenience init(_ string: String) {
-            let cups = string.map(String.init).compactMap(Int.init)
-            self.init(cups)
-        }
+        var cupAfter: [Int /* this cup */: Int /* Next cup */]
+        var highestCup: Int
         
         init(_ cups: [Int]) {
-            self.cups = cups
+            cupAfter = [:]
+            for (i, cup) in cups.enumerated() {
+                let nextCupIndex = (i + 1) % cups.count
+                let nextCup = cups[nextCupIndex]
+                cupAfter[cup] = nextCup
+            }
+            
             self.currentCup = cups[0]
+            self.highestCup = cups.max()!
         }
         
-        var indexOfCurrentCup: Int {
-            cups.firstIndex(of: currentCup)!
-        }
-        
-        var indexAfterCurrentCup: Int {
-            normalize(index: indexOfCurrentCup + 1)
-        }
-        
-        var destinationCup: Int {
-            return cups[destinationCupIndex]
-        }
-        
-        var destinationCupIndex: Int {
+        func destinationCup(excludingPickedUpCups pickedUpCups: [Int]) -> Int {
             var expectedCup = currentCup
             while true {
                 expectedCup -= 1
                 if (expectedCup < 1) {
-                    expectedCup = 9
+                    expectedCup = highestCup
                 }
                 
-                if let index = cups.firstIndex(of: expectedCup) {
-                    return index
+                if (!pickedUpCups.contains(expectedCup)) {
+                    return expectedCup
                 }
-            }
-        }
-        
-        func do100Moves() {
-            for _ in 0..<100 {
-                doMove()
             }
         }
         
         func doMove() {
-//            print("Cups: \(renderCups)")
-            
             let firstThreeCups = [
-                cups.remove(at: indexAfterCurrentCup),
-                cups.remove(at: indexAfterCurrentCup),
-                cups.remove(at: indexAfterCurrentCup),
+                cupAfter[currentCup]!,
+                cupAfter[cupAfter[currentCup]!]!,
+                cupAfter[cupAfter[cupAfter[currentCup]!]!]!,
             ]
             
-//            print("Taking 3 cups: \(firstThreeCups)")
-//            print("Destination Cup: \(destinationCup)")
+            let destination = destinationCup(excludingPickedUpCups: firstThreeCups)
+            let afterDestination = cupAfter[destination]
             
-            let indexAfterDestination = normalize(index: destinationCupIndex + 1)
-            cups.insert(contentsOf: firstThreeCups, at: indexAfterDestination)
+//            print("Taking 3 cups: \(firstThreeCups)")
+//            print("Destination Cup: \(destination)")
+            
+            
+            // fill in the gap where the three cups used to be
+            cupAfter[currentCup] = cupAfter[firstThreeCups[2]]
+            
+            // The destination cup now points to the first cup in the set we removed
+            cupAfter[destination] = firstThreeCups[0]
+            
+            // The cup after the third picked up cup points to the cup that was previously after the destination cup
+            cupAfter[firstThreeCups[2]] = afterDestination
+            
             rotateCurrentCup()
         }
         
         func rotateCurrentCup() {
-            currentCup = cups[indexAfterCurrentCup]
+            currentCup = cupAfter[currentCup]!
         }
         
-        func cup(at index: Int) -> Int {
-            cups[normalize(index: index)]
-        }
-        
-        func normalize(index: Int) -> Int {
-            index % cups.count
-        }
-        
-        var renderCups: String {
-            cups.map({ $0 == currentCup ? "(\($0))" : " \($0) "}).joined()
-        }
-        
-        var result: String {
-            let indexOf1 = cups.firstIndex(of: 1)!
-            let chars = ((indexOf1 + 1)..<(indexOf1 + cups.count))
-                .map(normalize(index:))
-                .map({ cups[$0] })
+        func solve1() -> String {
+            var output = [Int]()
+            var nextCup = cupAfter[1]!
+            for _ in (0..<8) {
+                output.append(nextCup)
+                nextCup = cupAfter[nextCup]!
+            }
+            
+           let chars = output
                 .compactMap(String.init)
                 .map(Character.init)
             
             return String(chars)
+        }
+        
+        var result: String {
+            let cupsAfter1 = [
+                cupAfter[1]!,
+                cupAfter[cupAfter[1]!]!,
+            ]
+            let product = cupsAfter1.reduce(1, *)
+            return "\(product)"
         }
     }
 }
