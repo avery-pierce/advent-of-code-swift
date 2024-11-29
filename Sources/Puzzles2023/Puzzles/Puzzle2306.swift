10f2884dda0cb466362b136883233bb0d41fba79
Avery Pierce <avery.pierce@protonmail.com>
2024-11-28 18:40:02 -0600

    Solve 2023 Day 6

--- Sources/Puzzles2023/Puzzles/Puzzle2306.swift
+++ Sources/Puzzles2023/Puzzles/Puzzle2306.swift
@@ -6,18 +6,86 @@ class Puzzle2306: Puzzle {
     let name: String = "2023_06"
     
     func solveA(_ input: Input) -> String {
-        return "unsolved"
+        let races = Race.from(input.lines)
+        let product = races
+            .map(\.winningCombinations)
+            .reduce(1, *)
+
+        return String(product)
     }
     
     var testCasesA: [TestCase] = [
-        // Input test cases here
+        TestCase(TextInput("""
+Time:      7  15   30
+Distance:  9  40  200
+"""), expectedOutput: "288")
     ]
     
     func solveB(_ input: Input) -> String {
-        return "unsolved"
+        let value = Race.flatten(input.lines).winningCombinations
+        return String(value)
     }
     
     var testCasesB: [TestCase] = [
-        // Input test cases here
+        TestCase(TextInput("""
+Time:      7  15   30
+Distance:  9  40  200
+"""), expectedOutput: "71503")
     ]
+    
+    struct Race {
+        var time: Int
+        var distance: Int
+        
+        static func from(_ input: [String]) -> [Race] {
+            let times = input[0].split(separator: " ")
+                .map(String.init)
+                .compactMap(Int.init)
+            
+            let distances = input[1].split(separator: " ")
+                .map(String.init)
+                .compactMap(Int.init)
+            
+            return zip(times, distances).map(Race.init)
+        }
+        
+        static func flatten(_ input: [String]) -> Race {
+            let timeString = input[0].split(separator: " ")
+                .map(String.init)
+                .compactMap(Int.init)
+                .map(String.init)
+                .joined()
+            
+            let time = Int(timeString)
+            
+            let distanceString = input[1].split(separator: " ")
+                .map(String.init)
+                .compactMap(Int.init)
+                .map(String.init)
+                .joined()
+            
+            let distance = Int(distanceString)
+            
+            return Race(time: time!, distance: distance!)
+        }
+        
+        init(time: Int, distance: Int) {
+            self.time = time
+            self.distance = distance
+        }
+        
+        var winningCombinations: Int {
+            (0..<time)
+                .map(distanceTraveled(whileHolding:))
+                .filter({ $0 > distance })
+                .count
+        }
+        
+        func distanceTraveled(whileHolding duration: Int) -> Int {
+            let speed = duration // 1 mm per ms
+            let remainingTime = time - duration
+            
+            return speed * remainingTime
+        }
+    }
 }
